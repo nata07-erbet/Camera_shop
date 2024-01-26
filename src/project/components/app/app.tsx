7755ed8079cbf2b8e23e6c19be4553c601c4d4eb
Nata <erbet@mail.ru>
2024-01-26 12:28:45 +0300

    feat: add-rewiew-pop-up

--- src/project/components/app/app.tsx
+++ src/project/components/app/app.tsx
@@ -1,11 +1,18 @@
 import { Catalog } from '../../pages/catalog/catalog';
 import { Product } from '../../pages/product/product';
+import { NotFoundPage } from '../../pages/404/404';
+import { Basket } from '../../pages/basket/basket';
+
+import { AddRewiew } from '../../pop-up/add-rewiew/add-rewiew';
 
 function App () {
   return(
     <>
       <Catalog />
       <Product />
+      <NotFoundPage />
+      <Basket />
+      <AddRewiew />
     </>
   );
 }
