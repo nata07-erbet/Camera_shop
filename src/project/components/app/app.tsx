fe419758c88641e1d99b2717fa28c5074983995b
Nata <erbet@mail.ru>
2024-01-26 13:45:12 +0300

    feat: add  delete-from-basket

--- src/project/components/app/app.tsx
+++ src/project/components/app/app.tsx
@@ -3,9 +3,6 @@ import { Product } from '../../pages/product/product';
 import { NotFoundPage } from '../../pages/404/404';
 import { Basket } from '../../pages/basket/basket';
 
-import { AddRewiew } from '../../components/add-rewiew/add-rewiew';
-import { RemoveFromBasket } from '../remove-from-basket/remove-from-basket';
-
 function App () {
   return(
     <>
@@ -13,8 +10,6 @@ function App () {
       <Product />
       <NotFoundPage />
       <Basket />
-      <AddRewiew />
-      <RemoveFromBasket />
     </>
   );
 }
