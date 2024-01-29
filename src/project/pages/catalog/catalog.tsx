a0988d1ec9162817ef59354cb348f69869ba6bf6
Nata <erbet@mail.ru>
2024-01-29 20:18:17 +0300

    add sorting-component

--- src/project/pages/catalog/catalog.tsx
+++ src/project/pages/catalog/catalog.tsx
@@ -6,6 +6,7 @@ import { ProductCardList } from '../../components/product-card-list/product-card
 import { Pangination } from '../../components/pangination/pangination';
 import { BreadCrumbs } from '../../components/breadcrumbs/breadcrumbs';
 import { Filter } from '../../components/filter/filter';
+import { Sorting } from '../../components/sorting/sorting';
 
 type CatalogProps = {
   products: TProducts;
@@ -32,58 +33,7 @@ function Catalog ({products}: CatalogProps) {
                 <Filter />
                 </div>
                 <div className="catalog__content">
-                  <div className="catalog-sort">
-                    <form action="#">
-                      <div className="catalog-sort__inner">
-                        <p className="title title--h5">Сортировать:</p>
-                        <div className="catalog-sort__type">
-                          <div className="catalog-sort__btn-text">
-                            <input
-                              type="radio"
-                              id="sortPrice"
-                              name="sort"
-                              defaultChecked
-                            />
-                            <label htmlFor="sortPrice">по цене</label>
-                          </div>
-                          <div className="catalog-sort__btn-text">
-                            <input type="radio" id="sortPopular" name="sort" />
-                            <label htmlFor="sortPopular">по популярности</label>
-                          </div>
-                        </div>
-                        <div className="catalog-sort__order">
-                          <div className="catalog-sort__btn catalog-sort__btn--up">
-                            <input
-                              type="radio"
-                              id="up"
-                              name="sort-icon"
-                              defaultChecked
-                              aria-label="По возрастанию"
-                            />
-                            <label htmlFor="up">
-                              <svg width={16} height={14} aria-hidden="true">
-                                <use xlinkHref="#icon-sort" />
-                              </svg>
-                            </label>
-                          </div>
-                          <div className="catalog-sort__btn catalog-sort__btn--down">
-                            <input
-                              type="radio"
-                              id="down"
-                              name="sort-icon"
-                              aria-label="По убыванию"
-                            />
-                            <label htmlFor="down">
-                              <svg width={16} height={14} aria-hidden="true">
-                                <use xlinkHref="#icon-sort" />
-                              </svg>
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-
+                  <Sorting />
                   <ProductCardList products={products} />
                   {isPaginationShow(products) ? (
                      <Pangination />
