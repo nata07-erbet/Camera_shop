cf96878d34396aea041727f7cef1cc83f518379c
Nata <erbet@mail.ru>
2024-02-08 21:24:00 +0300

    finish

--- src/project/pages/404/404.tsx
+++ src/project/pages/404/404.tsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const/const';
+
 function NotFoundPage() {
   return (
-    <p>404 Not found</p>
+    <>
+      <p>404 Not found</p>
+      <Link to={AppRoute.Main}>
+        Go to main Page
+      </Link>
+    </>
+
   );
 }
 
