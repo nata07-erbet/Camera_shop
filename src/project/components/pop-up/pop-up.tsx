7df0e400768a8ca194e416b2e8365a1be5c76c44
Nata <erbet@mail.ru>
2024-02-13 20:15:18 +0300

    add pop-up

---
+++ src/project/components/pop-up/pop-up.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+import { PropsWithChildren, useCallback, useEffect } from 'react';
+
+type PopUpProps = PropsWithChildren<{
+  onClose: () => void;
+  opened: boolean;
+  narrow: boolean;
+}>
+
+function PopUp({ children, onClose, opened, narrow}: PopUpProps) {
+
+  const handleOverlayClick = () => {
+    onClose?.();
+  };
+
+  const handleButtonCloseClick = () => {
+    onClose?.();
+  };
+
+
+  const handleEscapeKeyDown = useCallback(
+    (evt: KeyboardEvent) => {
+      if(evt.key === 'Escape') {
+        onClose?.();
+      }
+    },[onClose]);
+
+  useEffect(() =>{
+    document.addEventListener('keydown', handleEscapeKeyDown);
+
+    return () => document.removeEventListener('keydown', handleEscapeKeyDown);
+  }, [handleEscapeKeyDown]);
+
+  return (
+    <div
+      className={classNames('modal',
+        {'is-active': opened,
+          'modal-narrow': narrow
+        })}
+    >
+      <div className="modal__wrapper">
+        <div
+          className="modal__overlay"
+          onClick={handleOverlayClick}
+        >
+        </div>
+        <div className="modal__content">
+          {children}
+          <button className="cross-btn"
+            type="button"
+            aria-label="Закрыть попап"
+            onClick={handleButtonCloseClick}
+          >
+            <svg width={10} height={10} aria-hidden="true">
+              <use xlinkHref="#icon-close" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export type { PopUpProps};
+export { PopUp };
--- src/project/components/pop-up/popup-add-rewiew.tsx
+++ src/project/components/pop-up/popup-add-rewiew.tsx
@@ -26,123 +26,105 @@ function PopupAddRewiew ({onButtonClickPostRewiew, onKeyDownToClose, onButtonCli
   };
 
   return(
-    <div className="modal is-active">
-      <div className="modal__wrapper">
-        <div
-          className="modal__overlay"
-          onClick={handleClickOverlay}
+    <>
+      <p className="title title--h4">Оставить отзыв</p>
+      <div className="form-review">
+        <form
+          method="post"
+          onSubmit={handleFormSubmit}
+          onKeyDown={handleKeyDown}
         >
-        </div>
-        <div className="modal__content">
-          <p className="title title--h4">Оставить отзыв</p>
-          <div className="form-review">
-
-            <form
-              method="post"
-              onSubmit={handleFormSubmit}
-              onKeyDown={handleKeyDown}
-            >
-              <div className="form-review__rate">
-                <RatingRewiew />
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
+          <div className="form-review__rate">
+            <RatingRewiew />
+            <div className="custom-input form-review__item">
+              <label>
+                <span className="custom-input__label">
                       Ваше имя
-                      <svg width={9} height={9} aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake" />
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      placeholder="Введите ваше имя"
-                      minLength={2}
-                      maxLength={15}
-                      required
-                      autoFocus
-                    />
-                  </label>
-                  <p className="custom-input__error">Нужно указать имя</p>
-                </div>
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
+                  <svg width={9} height={9} aria-hidden="true">
+                    <use xlinkHref="#icon-snowflake" />
+                  </svg>
+                </span>
+                <input
+                  type="text"
+                  placeholder="Введите ваше имя"
+                  minLength={2}
+                  maxLength={15}
+                  required
+                  autoFocus
+                />
+              </label>
+              <p className="custom-input__error">Нужно указать имя</p>
+            </div>
+            <div className="custom-input form-review__item">
+              <label>
+                <span className="custom-input__label">
                       Достоинства
-                      <svg width={9} height={9} aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake" />
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      name="user-plus"
-                      placeholder="Основные преимущества товара"
-                      minLength={10}
-                      maxLength={160}
-                      required
-                    />
-                  </label>
-                  <p className="custom-input__error">Нужно указать достоинства</p>
-                </div>
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
+                  <svg width={9} height={9} aria-hidden="true">
+                    <use xlinkHref="#icon-snowflake" />
+                  </svg>
+                </span>
+                <input
+                  type="text"
+                  name="user-plus"
+                  placeholder="Основные преимущества товара"
+                  minLength={10}
+                  maxLength={160}
+                  required
+                />
+              </label>
+              <p className="custom-input__error">Нужно указать достоинства</p>
+            </div>
+            <div className="custom-input form-review__item">
+              <label>
+                <span className="custom-input__label">
                       Недостатки
-                      <svg width={9} height={9} aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake" />
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      name="user-minus"
-                      placeholder="Главные недостатки товара"
-                      minLength={10}
-                      maxLength={160}
-                      required
-                    />
-                  </label>
-                  <p className="custom-input__error">Нужно указать недостатки</p>
-                </div>
-                <div className="custom-textarea form-review__item">
-                  <label>
-                    <span className="custom-textarea__label">
+                  <svg width={9} height={9} aria-hidden="true">
+                    <use xlinkHref="#icon-snowflake" />
+                  </svg>
+                </span>
+                <input
+                  type="text"
+                  name="user-minus"
+                  placeholder="Главные недостатки товара"
+                  minLength={10}
+                  maxLength={160}
+                  required
+                />
+              </label>
+              <p className="custom-input__error">Нужно указать недостатки</p>
+            </div>
+            <div className="custom-textarea form-review__item">
+              <label>
+                <span className="custom-textarea__label">
                       Комментарий
-                      <svg width={9} height={9} aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake" />
-                      </svg>
-                    </span>
-                    <textarea
-                      name="user-comment"
-                      minLength={10}
-                      maxLength={160}
-                      placeholder="Поделитесь своим опытом покупки"
-                      defaultValue={''}
-                    />
-                  </label>
-                  <div className="custom-textarea__error">
+                  <svg width={9} height={9} aria-hidden="true">
+                    <use xlinkHref="#icon-snowflake" />
+                  </svg>
+                </span>
+                <textarea
+                  name="user-comment"
+                  minLength={10}
+                  maxLength={160}
+                  placeholder="Поделитесь своим опытом покупки"
+                  defaultValue={''}
+                />
+              </label>
+              <div className="custom-textarea__error">
                   Нужно добавить комментарий
-                  </div>
-                </div>
               </div>
-              <button
-                className="btn btn--purple form-review__btn"
-                type="submit"
-              >
-                Отправить отзыв
-              </button>
-            </form>
-
+            </div>
           </div>
-          <button className="cross-btn"
-            type="button"
-            aria-label="Закрыть попап"
-            onClick={handleClickClose}
+          <button
+            className="btn btn--purple form-review__btn"
+            type="submit"
           >
-            <svg width={10} height={10} aria-hidden="true">
-              <use xlinkHref="#icon-close" />
-            </svg>
+                Отправить отзыв
           </button>
-        </div>
+        </form>
+
       </div>
-    </div>
+
+    </>
 
   );
 }
