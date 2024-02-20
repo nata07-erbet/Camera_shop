811149cdea6fec92a0255f7814c2619dc501d02d
Nata <erbet@mail.ru>
2024-02-20 16:34:17 +0300

    add transformMode

--- src/project/components/rating/rating.test.tsx
+++ src/project/components/rating/rating.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { Rating } from './rating';
 import { INITAL_RATING } from '../../const/const';
 
-
 describe('component: Rating', () => {
   it('should render correctly', () => {
     const expectedText = 'Рейтинг';
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
 });
+
