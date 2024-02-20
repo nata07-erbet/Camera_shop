23267f011dd69b522942c2e9fa95c7231fe8341b
Nata <erbet@mail.ru>
2024-02-20 15:57:05 +0300

    add rating-rewiew.test

---
+++ src/project/components/rating/.tsx
@@ -0,0 +1,17 @@
+import { withHelmet } from '../../utils/mock-component/mock-component';
+import { render, screen } from '@testing-library/react';
+import { Rating } from './rating';
+import { INITAL_RATING } from '../../const/const';
+
+
+describe('component: Rating', () => {
+  it('should render correctly', () => {
+    const expectedText = 'Рейтинг';
+    const preparedComponent = withHelmet(<Rating rating={INITAL_RATING} />);
+
+    render(preparedComponent);
+
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+
+  });
+});
--- src/project/const/const.ts
+++ src/project/const/const.ts
@@ -61,7 +61,8 @@ const RatingMap = {
 } as const;
 
 const INITAL_PAGE_BY_PANGINATION = 1;
+const INITAL_RATING = '3';
 
 const ERROR_MESSAGE = 'Нужно оценить товар';
 
-export {PRODUCT_VIEW_COUNT, MOCK_START, MOCK_END, REWIEWS_COUNT, AppRoute, PanginationStep, INITAL_PAGE_BY_PANGINATION, PagesMap, RatingMap, AppRouteTab, DEFAULT_TAB, TabsMap, PanginationButton, PanginationsMap, ERROR_MESSAGE};
+export {PRODUCT_VIEW_COUNT, MOCK_START, MOCK_END, REWIEWS_COUNT, AppRoute, PanginationStep, INITAL_PAGE_BY_PANGINATION, PagesMap, RatingMap, AppRouteTab, DEFAULT_TAB, TabsMap, PanginationButton, PanginationsMap, ERROR_MESSAGE, INITAL_RATING};
