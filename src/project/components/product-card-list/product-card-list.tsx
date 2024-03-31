443ec19cf580c40f2bb867e744b2f2c48d8b6bb2
Nata <erbet@mail.ru>
2024-03-31 13:58:51 +0300

    fix product-list

--- src/project/components/product-card-list/product-card-list.tsx
+++ src/project/components/product-card-list/product-card-list.tsx
@@ -8,21 +8,24 @@ type ProductCardListProps = {
 
   isAddedBasket: boolean;
   isDisabled: boolean;
-}
+};
 
 function ProductCardList ({ products, onClickButton, onChangeDisabled, isAddedBasket, isDisabled }: ProductCardListProps) {
   return (
     <div className="cards catalog__cards" data-testid="product-container">
-      {products
-        .map((product) =>(
+      {products.length ? (
+        products.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
             onClickButton={onClickButton}
-            onChangeDisabled={onChangeDisabled}
-            isAddedBasket={isAddedBasket}
-            isDisabled={isDisabled}
-          />))}
+            isAddedBasket={false}
+            isDisabled={false}
+          />
+        ))
+      ) : (
+        <div>по вашему запросу ничего не найдено</div>
+      )}
     </div>
   );
 
